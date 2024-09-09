fadb5d4f9a8f913c01621adf28d134b5975a04e4
Helton Mori <heltonmori@gmail.com>
2024-09-09 15:57:05 -0300

    Aula 3 - classes index

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,41 @@
 <script setup>
-import { ref } from 'vue'
-import ProductDisplay from './components/ProductDisplay.vue'
+import { ref } from "vue";
+import ProductDisplay from "./components/ProductDisplay.vue";
 
-const cart = ref([])
-const premium = ref(true)
-  
+const cart = ref([]);
+const premium = ref(true);
+
 function updateCart(id) {
-  cart.value.push(id)
+  cart.value.push(id);
 }
 </script>
 
 <template>
-  <div class="nav-bar"></div>
+  <div class="h-16 mb-6 bg-gradient-to-r from-teal-500 to-[#84cf6a]"></div>
 
-  <div class="cart">Cart({{ cart.length }})</div>
-  <product-display :premium="premium" @add-to-cart="updateCart"></product-display>
+  <div
+    class="my-[25px] mx-[100px] p-[30px] border border-solid border-[#d8d8d8] bg-white float-right"
+  >
+    Cart({{ cart.length }})
+  </div>
+  <product-display
+    :premium="premium"
+    @add-to-cart="updateCart"
+  ></product-display>
 </template>
 
 <style scoped>
-
-.nav-bar {
+/* .nav-bar {
   height: 60px;
   margin-bottom: 25px;
   background: linear-gradient(90deg, #16c0b0, #84cf6a);
-}
+} */
 
-.cart {
+/* .cart {
   margin: 25px 100px;
   padding: 30px;
   border: 1px solid #d8d8d8;
   background-color: white;
   float: right;
-}
+} */
 </style>
